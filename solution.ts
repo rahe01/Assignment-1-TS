54a35496f087db4a669d7127fe4328f5985e96ee
Rahat Ahmed Rahe <130391604+rahe01@users.noreply.github.com>
2025-11-16 23:30:36 +0600

    Problem 1

--- solution.ts
+++ solution.ts
@@ -1,1 +1,21 @@
+const formatValue = (
+  param: string | number | boolean
+): string | number | boolean => {
+
+
+  if (typeof param === "string") {
+    return param.toUpperCase();
+  }
+
+
+  if (typeof param === "number") {
+    return param * 10;
+  }
+
+  if (typeof param === "boolean") {
+    return !param;
+  }
+
+  return param;
+};
 
