c87c88b4c82616002753bd778242c8c748ced523
Rahat Ahmed Rahe <130391604+rahe01@users.noreply.github.com>
2025-11-16 23:32:07 +0600

    P
    Added getLength function to return the length of a string or array.

--- solution.ts
+++ solution.ts
@@ -1,21 +1,27 @@
 const formatValue = (
   param: string | number | boolean
 ): string | number | boolean => {
-
-
   if (typeof param === "string") {
     return param.toUpperCase();
   }
-
-
   if (typeof param === "number") {
     return param * 10;
   }
-
   if (typeof param === "boolean") {
     return !param;
   }
-
   return param;
 };
 
+
+
+const getLength = (param: string | any[]): number => {
+
+    if (typeof param === 'string') {
+        return param.length;
+    }
+    else if (Array.isArray(param)) {
+        return param.length;
+    }
+    return 0;
+};
